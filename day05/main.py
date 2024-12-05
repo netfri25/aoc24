dec7aecb45dd31f8e7198f0cad61d9987a4cc7f1
Netanel Friedman <netanel.friedman6@gmail.com>
2024-12-05 23:17:12 +0200

    Add a python implementation of part 2 algorithm for day 05

---
+++ day05/main.py
@@ -0,0 +1,57 @@
+Update = list[int]
+Mappings = dict[int, set[int]]
+
+
+def parse_input(input: str) -> tuple[Mappings, list[Update]]:
+    mappings: dict[int, set[int]] = {}
+    while True:
+        if input[0] == "\n":
+            break
+
+        before, after = int(input[:2]), int(input[3:5])
+        input = input[6:]
+
+        if after not in mappings:
+            mappings[after] = set()
+        mappings[after].add(before)
+
+    input = input[1:]
+    updates = list(
+        map(
+            lambda line: list(map(int, line.split(","))),
+            input.strip().split(),
+        )
+    )
+    return mappings, updates
+
+
+def make_correct(update: list[int], mappings: Mappings):
+    update_index = 0
+    while update_index < len(update):
+        page = update[update_index]
+        if page not in mappings:
+            update_index += 1
+            continue
+        not_after = mappings[page]
+
+        for to_search in not_after:
+            try:
+                search = update.index(to_search, update_index)
+            except ValueError:
+                continue
+
+            temp = update[update_index]
+            update[update_index] = update[search]
+            update[search] = temp
+            break
+        else:
+            update_index += 1
+
+
+with open("example") as f:
+    mappings, updates = parse_input(f.read())
+    for update in updates:
+        print(update)
+        make_correct(update, mappings)
+        print(update)
+        print()
